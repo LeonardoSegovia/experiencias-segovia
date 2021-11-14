9149a8f11c131ecaab3e03c4ff6c3edac39ee257
Leonardo Segovia <segovia.leonardo.gustavo@gmail.com>
2021-11-13 23:01:21 -0300

    Fix Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,34 @@
-const NavBar = () =>{
-    return(<nav class="menu">
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </nav>)
-  
+import "./NavBar.css";
+import logo from "../logo.png"
+const NavBar = () => {
+  return (
+    <nav class="menu-top">
+      <div className="logo-container">
+        <img src={logo} alt="Vale por una imagen" />
+      </div>
+      <ul className="menu-nav">
+        <li>
+          <a href="#"> Inicio </a>
+        </li>
+      </ul>
+      <ul className="menu-list">
+        <li>
+          <a href="#">Vehiculos</a>
+        </li>
+        <li>
+          <a href="#">Electronica</a>
+        </li>
+        <li>
+          <a href="#">Libros</a>
+        </li>
+      </ul>
+      <ul className="menu-login">
+        <li>
+          <a href="#"> Login </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
